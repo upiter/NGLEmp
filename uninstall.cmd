b30588c31189cf1dc5f54b41b85b703227594fbb
NGLEmp <49843311+NGLEmp@users.noreply.github.com>
2020-12-31 11:44:13 +0000

    Sample uninstallation script
    Updated to Photoshop 2021

--- uninstall.cmd
+++ uninstall.cmd
@@ -1,5 +1,5 @@
 @echo off
-set PS_VERSION=2019
-set dest=%ProgramFiles%\Adobe\Adobe Photoshop CC %PS_VERSION%\Required\Plug-ins\Extensions
+set PS_VERSION=2021
+set dest=%ProgramFiles%\Adobe\Adobe Photoshop %PS_VERSION%\Required\Plug-ins\Extensions
 del /f "%dest%\NGLEmp.8bx"
 del /f "%dest%\painter.cfg"
