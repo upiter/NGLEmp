a375ca2f67d374cd63bcd7899b3178af31b42452
NGLEmp <49843311+NGLEmp@users.noreply.github.com>
2020-12-31 11:43:46 +0000

    Sample installation script
    Updated to Photoshop 2021

--- install.cmd
+++ install.cmd
@@ -1,5 +1,5 @@
 @echo off
-set PS_VERSION=2019
-set dest=%ProgramFiles%\Adobe\Adobe Photoshop CC %PS_VERSION%\Required\Plug-ins\Extensions
+set PS_VERSION=2021
+set dest=%ProgramFiles%\Adobe\Adobe Photoshop %PS_VERSION%\Required\Plug-ins\Extensions
 copy /b NGLEmp.8bx "%dest%"
 copy /b painter.cfg "%dest%"
